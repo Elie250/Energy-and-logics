e0e87a81d5dc7bf4b54677d23628faf1323ce4c7
Elie250 <103586757+Elie250@users.noreply.github.com>
2026-01-18 11:37:38 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
+@@ -0,0 +1,15 @@
 import React from "react"
-import Header from "../components/header" // <- relative path
-import Footer from "../components/footer" // <- relative path
+import Header from "@/components/header"
+import Footer from "@/components/footer"
 
 export default function RootLayout({ children }) {
   return (
