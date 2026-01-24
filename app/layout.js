33301320b20b8c3531722613e704b692101a87cd
Elie250 <103586757+Elie250@users.noreply.github.com>
2026-01-24 21:29:58 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,15 @@
-import Header from "../components/header"
-import Footer from "../components/footer"
+import "./globals.css";
+import Header from "../components/header";
+import Footer from "../components/footer";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
         <Header />
-        {children}
+        <main>{children}</main>
         <Footer />
       </body>
     </html>
-  )
+  );
 }
