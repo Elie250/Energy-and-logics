56185af8fa91120998881cbc5b98dbe2036a1655
Elie250 <103586757+Elie250@users.noreply.github.com>
2026-01-26 16:18:42 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,17 @@
 import "./globals.css";
-import Header from "../components/header";
-import Footer from "../components/footer";
+import Header from "@/components/header";
+import Footer from "@/components/footer";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className="min-h-screen flex flex-col bg-gray-50 text-slate-900">
         <Header />
-        <main>{children}</main>
+
+        <main className="flex-1 max-w-7xl mx-auto px-6 py-10">
+          {children}
+        </main>
+
         <Footer />
       </body>
     </html>
